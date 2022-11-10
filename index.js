28cf7f46d3d5dbe68cf801f585f6fbc99fae1192
salatik111 <115220008+salatik111@users.noreply.github.com>
2022-11-10 11:53:36 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+function checkAge(age) {
+  if (age >= 21) {
+    return true;
+  } else {
+    return confirm('У вас есть разрешение от ваших родителей?');
+  }
+}
+
+let age = prompt('Сколько вам лет?', 21);
+
+if ( checkAge(age) ) {
+  alert( 'Доступ получен' );
+} else {
+  alert( 'Доступ отклонён' );
+}
